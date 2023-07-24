a78e833af7a9497312334fac8edacdb3aa275eb7
Daniel Castillo <danielcastillo89@icloud.com>
2023-07-24 17:42:20 -0500

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,72 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const md = require("./accounts-middleware");
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json("get account");
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.get("/:id", md.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    res.json("get account");
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post(
+  "/",
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("post account");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  md.checkAccountId,
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("update account");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete(
+  "/:id",
+  md.checkAccountId,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("delete account");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   // DO YOUR MAGIC
-})
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
 
 module.exports = router;
